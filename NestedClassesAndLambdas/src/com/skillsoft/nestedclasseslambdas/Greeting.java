05ea43aa80a68788fd1b19925c985685dbed9fd4
Juan Villa <ing.villa.jimenez@gmail.com>
2023-07-29 22:31:34 -0600

    Implementing Functional Interfaces Using Lambdas

--- NestedClassesAndLambdas/src/com/skillsoft/nestedclasseslambdas/Greeting.java
+++ NestedClassesAndLambdas/src/com/skillsoft/nestedclasseslambdas/Greeting.java
@@ -2,8 +2,11 @@ package com.skillsoft.nestedclasseslambdas;
 
 public interface Greeting {
 
-    String greetMultipleTimes(String name, String customGreeting, int numTimes);
-//    String customGreetingByName(String name, String customGreeting);
-//    String simpleGreetingByName(String name);
+    String simpleGreetingByName(String name); // abstract method
+
+    default String simpleGreeting() { // default implementation
+        return "Default greeting!";
+    }
+
 //    String simpleGreeting();
 }
--- NestedClassesAndLambdas/src/com/skillsoft/nestedclasseslambdas/Main.java
+++ NestedClassesAndLambdas/src/com/skillsoft/nestedclasseslambdas/Main.java
@@ -2,132 +2,60 @@ package com.skillsoft.nestedclasseslambdas;
 
 public class Main {
 
-    public static void greet(String name, String customGreeting, int numTimes, Greeting greeting) {
-        System.out.println(greeting.greetMultipleTimes(name, customGreeting, numTimes));
-    }
-
-//    public static void greet(String name, String customGreeting, Greeting greeting) {
-//        System.out.println(greeting.customGreetingByName(name, customGreeting));
-//    }
-
-//    public static void greet(String name, Greeting greeting) {
-//        System.out.println(greeting.simpleGreetingByName(name));
-//    }
-
-//    public static void greet(Greeting greeting) {
-//        System.out.println(greeting.simpleGreeting());
-//    }
-
     public static void main(String[] args) {
 
-        greet("John", "Good morning!", 3, new Greeting() {
+        Greeting simpleGreetingAnonymous = new Greeting() {
             @Override
-            public String greetMultipleTimes(String name, String customGreeting, int numTimes) {
-
-                String greeting = String.format("%s %s\n", customGreeting, name);
-
-                StringBuilder sb = new StringBuilder();
-
-                for (int i = 0; i < numTimes; i++) {
-                    sb.append(greeting);
-                }
-
-                return sb.toString();
+            public String simpleGreetingByName(String name) {
+                return "Hello, " + name;
             }
-        });
+        };
 
-        greet("Carrie", "Welcome aboard!", 2,
-                (String name, String customGreeting, int numTimes) ->
-                        String.format("**%s %s**\n", customGreeting, name).repeat(numTimes));
+        System.out.println(simpleGreetingAnonymous.simpleGreetingByName("Alice"));
 
-        greet("Ronald", "Good evening, how are you today?", 3,
-                (String name, String customGreeting, int numTimes) ->
-                        String.format("--%s %s--\n", customGreeting, name).repeat(numTimes));
+        Greeting simpleGreetingLambdaWithTypes = (String name) -> "Good morning, " + name;
 
-//        greet("Carrie", "Welcome aboard!", 2,
-//                (name, customGreeting, numTimes) ->
-//                        String.format("**%s %s**\n", customGreeting, name).repeat(numTimes)); // lambda expression
-//
-//        greet("Ronald", "Good evening, how are you today?", 3,
-//                (name, customGreeting, numTimes) ->
-//                        String.format("--%s %s--\n", customGreeting, name).repeat(numTimes)); // lambda expression
+        System.out.println(simpleGreetingLambdaWithTypes.simpleGreetingByName("Bob"));
 
-//        greet("Carrie", "Welcome aboard!", 2,
-//                (name, customGreeting, numTimes) -> {
-//                    return String.format("**%s %s**\n", customGreeting, name).repeat(numTimes); // Statement lambda can be replaced with expression lambda
-//                });
-//
-//        greet("Ronald", "Good evening, how are you today?", 3,
-//                (name, customGreeting, numTimes) -> {
-//                    return String.format("--%s %s--\n", customGreeting, name).repeat(numTimes); // Statement lambda can be replaced with expression lambda
-//                });
+        Greeting simpleGreetingLambdaWithoutTypes = (name) -> "Welcome, " + name;
 
-//        greet("Carrie", "Welcome aborad!", 2,
-//                (name, customGreeting, numTimes) -> {
-//                    String greeting = String.format("**%s %s**\n", customGreeting, name);
-//
-//                    StringBuilder sb = new StringBuilder();
-//
-//                    for (int i = 0; i < numTimes; i++) {
-//                        sb.append(greeting);
-//                    }
-//
-//                    return sb.toString();
-//                });
-//
-//        greet("Ronald", "Good evening, how are you today?", 3,
-//                (name, customGreeting, numTimes) -> {
-//                    String greeting = String.format("--%s %s--\n", customGreeting, name);
-//
-//                    StringBuilder sb = new StringBuilder();
-//
-//                    for (int i = 0; i < numTimes; i++) {
-//                        sb.append(greeting);
-//                    }
-//
-//                    return sb.toString();
-//                });
+        System.out.println(simpleGreetingLambdaWithoutTypes.simpleGreetingByName("Charlie"));
 
-//        greet("John", "Good morning!", new Greeting() {
-//            @Override
-//            public String customGreetingByName(String name, String customGreeting) {
-//                return (customGreeting + " " + name);
-//            }
-//        });
-//
-//        greet("Carrie", "Welcome aboard!",
-//                (name, customGreeting) -> customGreeting + " " + name);
-//
-//        greet("Ronald", "Good evening, how are you today?",
-//                (name, customGreeting) -> name + " " + customGreeting);
+        Greeting simpleGreetingLambdaWithoutParenthesis = name -> "Howdy, " + name;
 
-//        greet("John", new Greeting() {
-//            @Override
-//            public String simpleGreetingByName(String name) {
-//                return ("Hello " + name);
-//            }
-//        });
-//
-//        greet("Carrie", name -> "Good morning! " + name);
-//
-//        greet("Ronald", name -> name + " It's a beautiful day!");
+        System.out.println(simpleGreetingLambdaWithoutParenthesis.simpleGreetingByName("Donna"));
 
-//        greet("Carrie", (name) -> "Good morning! " + name);
-//
-//        greet("Ronald", (name) -> name + " It's a beautiful day!");
+        Greeting simpleGreetingLambdaWithBlock = name -> {
+            name = name.toUpperCase();
+            return "Hi there, " + name;
+        };
 
-//        greet(new Greeting() {
+        System.out.println(simpleGreetingLambdaWithBlock.simpleGreetingByName("Elise"));
+
+        System.out.println("\n*****");
+        System.out.println(simpleGreetingAnonymous.simpleGreeting());
+        System.out.println(simpleGreetingLambdaWithTypes.simpleGreeting());
+        System.out.println(simpleGreetingLambdaWithoutTypes.simpleGreeting());
+        System.out.println(simpleGreetingLambdaWithoutParenthesis.simpleGreeting());
+        System.out.println(simpleGreetingLambdaWithBlock.simpleGreeting());
+
+//        Greeting simpleGreetingAnonymous = new Greeting() {
 //            @Override
 //            public String simpleGreeting() {
-//                return "Hello world!";
+//                return "Hello world from an anonymous function!";
 //            }
-//        });
+//        };
 //
-//        greet(() -> "Good morning!");
+//        Greeting simpleGreetingLambda = () -> "Hello world from a lambda expression!";
 //
-//        greet(() -> "Here is a simple interface method which takes in no parameters!");
+//        System.out.println(simpleGreetingAnonymous.simpleGreeting());
+//        System.out.println(simpleGreetingLambda.simpleGreeting());
+
     }
 }
 
 // Lambda Expressions
 // A short block of code which takes in input arguments and performs an action or returns a value
+
+// Functional Interface
+// An interface with exactly one abstract method
